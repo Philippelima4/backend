ecff889671ed00c45ec750698b339d4f1e5311fc
Philippelima4 <philippeantonio20@gmail.com>
2025-12-03 17:37:31 +0000

    a

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
 export default () => ({});
+// Localização: config/plugins.js
+module.exports = ({ env }) => ({
+  // === ESTE É O NOVO CÓDIGO ===
+  'users-permissions': {
+    config: {
+      // Lê a variável de ambiente 'JWT_SECRET' para o login/registo
+      jwtSecret: env('JWT_SECRET'), 
+    },
+  },
+  // Se tiveres outros plugins configurados, adiciona-os aqui
+});
