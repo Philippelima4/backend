59c18ba3e3f6821b541bc8ececa0946e2e089916
Philippelima4 <philippeantonio20@gmail.com>
2025-12-03 17:26:17 +0000

    as

--- config/server.ts
+++ config/server.ts
@@ -14,3 +14,16 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
 });
+
+// Altera este ficheiro no teu Backend (Strapi)
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  
+  // === ESTE É O NOVO CÓDIGO ===
+  app: {
+    // Isto diz ao Strapi para usar a variável APP_KEYS que definiste no Render
+    keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
+  },
+  // ============================
+});
