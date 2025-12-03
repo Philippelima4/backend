aaea8a3dc5c0fb547c5b02b160413b7648fa52c1
Philippelima4 <philippeantonio20@gmail.com>
2025-12-03 18:10:20 +0000

    a

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,7 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
+      // Lê a variável de ambiente JWT_SECRET
       jwtSecret: env('JWT_SECRET'),
     },
   },
